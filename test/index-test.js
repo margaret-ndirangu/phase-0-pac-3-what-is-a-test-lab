a50331e7a22f18c8e16eb9fc958e942e88170f2c
Margaret <wanjirundirangu43@gmail.com>
2022-06-03 17:35:20 +0300

    commit tree b652e001d07dc3cf74c8128d130ff40bce6ffeb7

--- test/index-test.js
+++ test/index-test.js
@@ -2,15 +2,15 @@ const { name, height, message } = require("../index.js");
 
 describe("what-is-a-test", () => {
   describe("Name", () => {
-    it('returns "Joe"', () => {
-      expect(name).toEqual("Joe");
+    it('returns "Susan"', () => {
+      expect(name).toEqual("Susan");
 
     });
   });
 
   describe("Height", () => {
-    it("is equal to  74", () => {
-      expect(height).toEqual(74);
+    it("is less than 40", () => {
+      expect(height).toBeLessThan(40);
     });
   });
 
